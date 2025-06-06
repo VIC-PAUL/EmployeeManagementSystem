c7f5dd1f8682a7c38281127e68d529f1efe7223d
Victor Paul Choudhury <vicpaul1994@gmail.com>
2025-06-06 11:25:50 +0530

    Update AuthProvider.jsx

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react'
 import { getLocalStorage, setLocalStorage } from '../utils/localStorage';
 
+
 export const AuthContext = createContext()
 
 const AuthProvider = ({children}) => {
