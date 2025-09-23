5a755d81d7b759af567f461bd6dfe71e9bf34947
Victor Paul Choudhury <vicpaul1994@gmail.com>
2025-09-23 13:03:38 +0530

    Update EmployeeDashboard.jsx

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -3,7 +3,6 @@ import Header from '../Other/Header'
 import TaskListNumbers from '../Other/TaskListNumbers'
 import TaskList from '../TaskList/TaskList'
 
-
 const EmployeeDashboard = (props) => {
   return (
     <div className='p-10 bg-[#1C1C1C] h-screen'>
